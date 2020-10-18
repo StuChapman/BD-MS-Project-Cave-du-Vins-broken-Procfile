cb9b2801ad43fa3057bdb8ae376a5a5e80c12b1d
Stuart Chapman <chapman.stuart@sky.com>
2020-10-18 15:49:48 +0000

    Build: added populate_search route to run.py to pre-populate dropdowns on index.html

--- run.py
+++ run.py
@@ -5,16 +5,21 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0.5zsef.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_DBNAME"] = 'CaveDuVin'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0.5zsef.mongodb.net/CaveDuVin?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/get_tasks')
-def get_tasks():
-    return render_template("index.html", tasks=mongo.db.tasks.find())
+@app.route('/populate_search')
+def populate_search():
+    return render_template("index.html", colours=mongo.db.colours.find(), country=mongo.db.country.find(), region=mongo.db.region.find())
+
+
+@app.route('/filter_region')
+def filter_region():
+    return render_template("index.html", region=mongo.db.region.find("country: France"))
 
 
 if __name__ == '__main__':
