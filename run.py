f579366064ee0df3f99641a5201b7d0530fb3333
Stuart Chapman <chapman.stuart@sky.com>
2020-10-19 17:50:36 +0000

    Build: moved mongodb connection string into env.py

--- run.py
+++ run.py
@@ -3,10 +3,13 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+if os.path.exists("env.py"):
+    import env
+
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'CaveDuVin'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0.5zsef.mongodb.net/CaveDuVin?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
 
