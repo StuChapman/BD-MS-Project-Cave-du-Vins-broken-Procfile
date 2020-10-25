ae18c0cc7bfe5497d7922a022c540b3428f4051f
Stuart Chapman <chapman.stuart@sky.com>
2020-10-25 16:39:54 +0000

    Build: added signup collection, route and modal

--- run.py
+++ run.py
@@ -27,8 +27,18 @@ def search():
     coloursearch = request.values.get("colour")
     countrysearch = request.values.get("country")
     regionsearch = request.values.get("region")
-    return render_template("index.html", results=mongo.db.wines.find( {"$and": [ {"wine_name": namesearch}, {"vintage": vintagesearch}, {"colour": coloursearch}, {"country": countrysearch} , {"region": regionsearch} ] }))
+    return render_template("index.html", results=mongo.db.wines.find({"$text": {"$search": namesearch}}))
+    # return render_template("index.html", results=mongo.db.wines.find( {"$and": [ {"wine_name": namesearch}, {"vintage": vintagesearch}, {"colour": coloursearch}, {"country": countrysearch} , {"region": regionsearch} ] }))
+    redirect(url_for('populate_search'))
 
+
+@app.route("/add_user", methods=["GET", "POST"])
+def add_user():
+    usernameinput = request.values.get("username")
+    passwordinput = request.values.get("password")
+    users = mongo.db.users
+    users.insert_one({"username": usernameinput, "password": passwordinput})
+    return redirect(url_for('populate_search'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
