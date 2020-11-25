6962f4cbe462f3d9f9157396dc5daf5456ca7125
Stuart Chapman <chapman.stuart@sky.com>
2020-11-25 20:01:48 +0000

    Test: tested uploading an image to azure

--- run.py
+++ run.py
@@ -489,31 +489,25 @@ def upload_image(wine_id):
     # environment variable into account.
     
     connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
-    print(connect_str)
 
     # Create the BlobServiceClient object which will be used to create a container client
     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
     # Create a unique name for the container
-    container_name = "quickstart" + str(uuid.uuid4())
+    container_name = wine_id
 
     # Create the container
     container_client = blob_service_client.create_container(container_name)
 
+    # Open a File Box
+
     # Create a file in local data directory to upload and download
     local_path = "./data"
-    local_file_name = "quickstart" + str(uuid.uuid4()) + ".txt"
+    local_file_name = "wine.jpg"
     upload_file_path = os.path.join(local_path, local_file_name)
-
-    # Write text to the file
-    file = open(upload_file_path, 'w')
-    file.write("Hello, World!")
-    file.close()
 
     # Create a blob client using the local file name as the name for the blob
     blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
-
-    print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
 
     # Upload the created file
     with open(upload_file_path, "rb") as data:
